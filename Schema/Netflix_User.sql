2658bb4b279581e1f1d2d5c5af23a935466359d2
sinapy <sinasarsharpour@cmail.carleton.ca>
2023-03-22 09:28:56 -0400

    initial commit

---
+++ Schema/Netflix_User.sql
@@ -0,0 +1,8 @@
+CREATE TABLE USER(
+    userId NUMBER,
+    firstName VARCHAR2(30),
+    lastName VARCHAR2(30),
+    emailAddress VARCHAR2(100),
+    hashPassword VARCHAR2(200),
+    age NUMBER(3, 0)
+)
