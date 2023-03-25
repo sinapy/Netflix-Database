be362f6fed6903e58937af9113d33ef00d0e0c82
BarathSrinivasan2002 <56555882+BarathSrinivasan2002@users.noreply.github.com>
2023-03-25 11:45:28 -0400

    Update Schema/Netflix_Movie.sql
    Co-authored-by: sinapy <47465748+sinapy@users.noreply.github.com>

--- Schema/Netflix_Movie.sql
+++ Schema/Netflix_Movie.sql
@@ -1,7 +1,8 @@
 CREATE TABLE netflix_movie (
   movieID NUMBER(10) PRIMARY KEY,
   Title VARCHAR2(40) NOT NULL ,
-  Rating VARCHAR2 (10),
-  CHECK(Rating = 'like' OR Rating = 'dislike'),
+  Rating NUMBER (1,1),
+  CHECK(Rating > 0 AND Rating < 10),
+
   languageID NUMBER(10)
 )
