8da47e0f0078ecc2dcc50804ee54e191490fcacf
BarathSrinivasan2002 <56555882+BarathSrinivasan2002@users.noreply.github.com>
2023-03-25 10:59:59 -0400

    Update Netflix_Movie.sql
    adding movie table

---
+++ Schema/Netflix_Movie.sql
@@ -0,0 +1,7 @@
+CREATE TABLE movie (
+  movieID NUMBER(10) PRIMARY KEY,
+  Title VARCHAR2 NOT NULL,
+  Rating VARCHAR2,
+  CONSTRAINT CHECK(Ratings = like OR Ratings = dislike),
+  languageID NUMBER(10)
+)
