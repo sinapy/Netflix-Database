91d37e92b69d5e990ed77414e645bed57f2b2d81
BarathSrinivasan2002 <56555882+BarathSrinivasan2002@users.noreply.github.com>
2023-03-25 11:52:25 -0400

    Update Netflix_Movie.sql

--- Schema/Netflix_Movie.sql
+++ Schema/Netflix_Movie.sql
@@ -1,8 +1,11 @@
 CREATE TABLE netflix_movie (
   movieID NUMBER(10) PRIMARY KEY,
   Title VARCHAR2(40) NOT NULL ,
-  Rating NUMBER (1,1),
+  Rating VARCHAR2 (10),
   CHECK(Rating > 0 AND Rating < 10),
-
-  languageID NUMBER(10)
+  languageID NUMBER(10),
+  Constraint FK_movieId_movies FOREIGN KEY (languageID) REFERENCES netflix_language(languageID)
 )
+
+
+
