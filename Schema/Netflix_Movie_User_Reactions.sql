330786f77ad4a8a52d47acf002b0de93d917656a
BarathSrinivasan2002 <56555882+BarathSrinivasan2002@users.noreply.github.com>
2023-03-25 11:50:36 -0400

    Update Netflix_Movie_User_Reactions.sql

--- Schema/Netflix_Movie_User_Reactions.sql
+++ Schema/Netflix_Movie_User_Reactions.sql
@@ -4,5 +4,5 @@ CREATE TABLE netflix_movie_user_reaction(
     userID NUMBER(10),    
     Constraint FK_movieId_movie FOREIGN KEY (movieID) REFERENCES netflix_movie(movieID),
     Constraint FK_reactionID_reaction FOREIGN KEY (reactionID) REFERENCES netflix_reactions(ReactionID),
-    Constraint FK_userID_reaction FOREIGN KEY (userID) REFERENCES netflix_users(id)
+    Constraint FK_userID_reaction FOREIGN KEY (userID) REFERENCES netflix_users(userId)
     )
